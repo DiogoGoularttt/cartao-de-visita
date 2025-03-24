96eba1708ba839d895c155bdc3a55f323877d68b
Diogo Goulart <136640390+DiogoGoularttt@users.noreply.github.com>
2025-03-24 02:51:30 -0300

    :sparkles: feat: Add download button enabled only after generating the card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { heroPhoneSolid, heroEnvelopeSolid, heroMapPinSolid } from '@ng-icons/heroicons/solid';
+import {
+  heroPhoneSolid,
+  heroEnvelopeSolid,
+  heroMapPinSolid,
+} from '@ng-icons/heroicons/solid';
 import { CommonModule } from '@angular/common';
-import { NgxMaskDirective, provideNgxMask  } from 'ngx-mask'; 
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 import {
   FormBuilder,
   FormGroup,
@@ -14,7 +20,10 @@ import {
   selector: 'app-root',
   standalone: true,
   imports: [ReactiveFormsModule, CommonModule, NgxMaskDirective, NgIcon],
-  providers: [provideNgxMask(), provideIcons({ heroEnvelopeSolid, heroMapPinSolid, heroPhoneSolid })],
+  providers: [
+    provideNgxMask(),
+    provideIcons({ heroEnvelopeSolid, heroMapPinSolid, heroPhoneSolid }),
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -22,6 +31,7 @@ export class AppComponent {
   form: FormGroup;
   showCard = false;
   isFlipped = false;
+  isCardGenerated = false; 
 
   flipCard() {
     this.isFlipped = !this.isFlipped;
@@ -37,7 +47,7 @@ export class AppComponent {
       number: ['', Validators.required],
       neighborhood: ['', Validators.required],
       city: ['', Validators.required],
-      backgroundColor: ['#ffffff'], 
+      backgroundColor: ['#ffffff'],
       textColor: ['#000000'],
       iconColor: ['#000000'],
     });
@@ -49,9 +59,15 @@ export class AppComponent {
     const numeros = phone.replace(/\D/g, '');
 
     if (numeros.length === 11) {
-      return `(${numeros.substring(0, 2)}) ${numeros.substring(2, 7)}-${numeros.substring(7)}`;
+      return `(${numeros.substring(0, 2)}) ${numeros.substring(
+        2,
+        7
+      )}-${numeros.substring(7)}`;
     } else if (numeros.length === 10) {
-      return `(${numeros.substring(0, 2)}) ${numeros.substring(2, 6)}-${numeros.substring(6)}`;
+      return `(${numeros.substring(0, 2)}) ${numeros.substring(
+        2,
+        6
+      )}-${numeros.substring(6)}`;
     }
 
     return phone;
@@ -60,6 +76,77 @@ export class AppComponent {
   gerarCartao() {
     if (this.form.valid) {
       this.showCard = true;
+      this.isCardGenerated = true;
+    }
+  }
+
+  removeRotation(element: HTMLElement) {
+    element.style.transform = 'rotateY(0deg)';
+  }
+
+  restoreRotation(element: HTMLElement) {
+    element.style.transform = 'rotateY(180deg)';
+  }
+
+  downloadPDF() {
+    const cardFrontElement = document.querySelector(
+      '.card-front'
+    ) as HTMLElement;
+    const cardBackElement = document.querySelector('.card-back') as HTMLElement;
+
+    if (cardFrontElement && cardBackElement) {
+      this.removeRotation(cardBackElement);
+
+      html2canvas(cardFrontElement, {
+        backgroundColor: this.form.value.backgroundColor,
+      }).then((frontCanvas) => {
+        const frontImgData = frontCanvas.toDataURL('image/png');
+
+        html2canvas(cardBackElement, {
+          backgroundColor: this.form.value.backgroundColor,
+        }).then((backCanvas) => {
+          const backImgData = backCanvas.toDataURL('image/png');
+
+          this.restoreRotation(cardBackElement);
+          const pdf = new jsPDF('p', 'mm', 'a4');
+
+          const margin = 10;
+          const imgWidth = 190;
+          const frontImgHeight =
+            (frontCanvas.height * imgWidth) / frontCanvas.width;
+          const backImgHeight =
+            (backCanvas.height * imgWidth) / backCanvas.width;
+
+          pdf.addImage(
+            frontImgData,
+            'PNG',
+            margin,
+            margin,
+            imgWidth,
+            frontImgHeight
+          );
+
+          pdf.rect(margin - 1, margin - 1, imgWidth + 2, frontImgHeight + 2);
+
+          pdf.addImage(
+            backImgData,
+            'PNG',
+            margin,
+            margin * 2 + frontImgHeight,
+            imgWidth,
+            backImgHeight
+          );
+
+          pdf.rect(
+            margin - 1,
+            margin * 2 + frontImgHeight - 1,
+            imgWidth + 2,
+            backImgHeight + 2
+          );
+
+          pdf.save(`${this.form.value.name}_cartao_de_visitas.pdf`);
+        });
+      });
     }
   }
 }
